6adbeeea0470b2982f6f7cf8ed1d6324f5351e18
Kai Yonashiro <kai.yonashiro@gmail.com>
2022-06-09 09:47:33 -1000

    completed

--- server/index.js
+++ server/index.js
@@ -1,21 +1,46 @@
-const port = process.env.PORT;
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
+const port = process.env.PORT;
+const { CONNECTION_STRING } = process.env;
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 app.use(express.json());
 
-const cors = require('cors')
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 
 
-app.get()
+app.get("/api/info", async (req, res) => {
+  let info = await sequelize
+    .query(
+      `
+        SELECT * FROM input 
+    `
+    )
+    res.status(200).send(info[0]);
+});
+
+app.post("/api/info", async (req, res) => {
+    let info = await sequelize.query(`
+    INSERT INTO input(text)
+    VALUES('${req.body.text}')
+    `)
+    res.status(200).send(info[0])
+})
+
 
 
 app.listen(port, () => {
-    console.log(`App listening on port ${process.env.PORT}`);
-  });
-
-
-
+  console.log(`App listening on port ${port}`);
+});
